2c9d3383a79e833b02c1671e191f05385a913164
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2024-11-20 22:02:13 +0530

    Update

---
+++ Example for Intersection and Union.py
@@ -0,0 +1,11 @@
+# Define two sets
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+# Intersection
+intersection = A & B
+print("Intersection:", intersection)  # Output: {3, 4}
+
+# Union
+union = A | B
+print("Union:", union)  # Output: {1, 2, 3, 4, 5, 6}
